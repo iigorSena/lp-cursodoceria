d760ed11e9721d7e3fe1c7cd2c891c972643b354
iigorSena <igor_senna@outlook.com.br>
2025-06-17 18:32:35 -0300

    add scrippt de inscrição

--- js/script.js
+++ js/script.js
@@ -23,3 +23,40 @@ const contadorObserver = new IntersectionObserver((entries, obs) => {
 });
 
 contadorObserver.observe(document.getElementById('quantidade-alunos'));
+
+
+// Controle de form
+const form = document.getElementById('inscricaoForm');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const hoje = new Date();
+  const dataFormatada = hoje.toLocaleDateString('pt-BR'); // ou use ISO se preferir
+
+  const dados = {
+    inscricao: {
+      nome: form.querySelector('[name="nome"]').value,
+      email: form.querySelector('[name="email"]').value,
+      dataInscricao: dataFormatada // ⚠️ tem que bater com o nome da coluna
+    }
+  };
+
+  fetch('https://api.sheety.co/686e96cab121db1f167d70a4ae2e4745/inscricoesCurso/inscricao', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dados)
+  })
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    document.getElementById('resposta').textContent = 'Inscrição enviada com sucesso!';
+    form.reset();
+  })
+  .catch(err => {
+    console.error(err);
+    document.getElementById('resposta').textContent = 'Erro ao enviar. Tente novamente.';
+  });
+});
